7e61147d2cd08d3bfe9f381d9e1d1fa649764e64
Revathi-j <revathi.blisslogix@gmail.com>
2016-04-07 15:19:16 +0530

    122323

--- PercentageCalculatorTests/PercentageCalculatorTests.swift
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -24,17 +24,23 @@ class PercentageCalculatorTests: XCTestCase {
     }
     
     func testExample() {
+
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
     }
     func testLabelValuesShowedProperly() {
+        
         let _ = vc.view
         vc.updateLabels(Float(80.0), Float(50.0), Float(40.0))
         
         // The labels should now display 80, 50 and 40
+        
         XCTAssert(vc.numberLabel.text == "80.0", "numberLabel doesn't show the right text")
         XCTAssert(vc.percentageLabel.text == "50.0%", "percentageLabel doesn't show the right text")
+
        // XCTAssert(vc.resultLabel.text == "40.0", "resultLabel doesn't show the right text")
+        
     }
     
     func testPerformanceExample() {
